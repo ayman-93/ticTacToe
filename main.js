30a32143511af0813d6a86a141fee24956f849ab
ayman-93 <aymanfaisal.cs@gmail.com>
2019-01-27 10:06:46 +0300

    modified the function play to see who is turn

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
+let playerOne = {
+  name: "",
+  wins: 0,
+  turn: true
+};
+
+let playerTwo = {
+  name: "",
+  wins: 0,
+  turn: false
+};
 let playArea = $(".playArea");
 
 function play(event) {
   let square = event.target;
-  console.log(square);
-  $(square).addClass("x");
+  if (playerOne.turn) {
+    console.log(square);
+    $(square).addClass("x");
+    playerOne.turn = false;
+  } else {
+    $(square).addClass("o");
+    playerOne.turn = true;
+  }
 }
+
 playArea.on("click", play);
