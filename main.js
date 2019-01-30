bb9f94a033b82a84a175abdecd6b09e2f8576f76
ayman-93 <aymanfaisal.cs@gmail.com>
2019-01-30 23:39:46 +0300

    fix some bugs and added some features

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ let computer = {
   stack: [],
   wins: 0,
   turn: false,
-  level: "medium"
+  level: "hard"
 };
 let gameOver = false;
 let played = [];
@@ -31,6 +31,7 @@ let winPattrens = [
   ["1", "5", "9"],
   ["3", "5", "7"]
 ];
+let woosh = $("#woosh");
 //functuon to check the winner every time somone play its run
 function checkWin(playerStack) {
   let match = 0;
@@ -48,12 +49,13 @@ function checkWin(playerStack) {
   }
 }
 
-//to check if the game is tie it's run after all the square is played
+//to check if the game is tie, it's run after all the square is played
 function tie(first, second) {
   if (
     (first.stack.length === 5 && second.stack.length === 4) ||
     (first.stack.length === 4 && second.stack.length === 5)
   ) {
+    gameOver = true;
     $("#playerName").text("");
     $("#wins").replaceWith("<p id='wins'>TIE</p>");
 
@@ -68,10 +70,12 @@ function tie(first, second) {
 }
 
 //this function take the players names and slide up the first layer its run on the beginning of the game only
-function startGame(event, first, second) {
+function startGame(first, second) {
   if ($("#playerOneN").val()) first.name = $("#playerOneN").val();
   if ($("#playerTwoN").val()) second.name = $("#playerTwoN").val();
   let score = $(".score");
+  let options = $(".options");
+
   score.append(
     `<p id='scoreOne' >${first.name}: ${first.wins}</p><p id='scoreTwo'>${
       second.name
@@ -79,12 +83,14 @@ function startGame(event, first, second) {
   );
   $(".ready").slideUp();
   score.slideDown();
+  options.slideDown();
 }
 //this function reset play area and the players stacks and update the scores
-function playAgain(event, first, second) {
+function playAgain(first, second) {
   first.stack = [];
   second.stack = [];
   played = [];
+  gameOver = false;
 
   $(".square").removeClass("x");
   $(".square").removeClass("o");
@@ -95,10 +101,11 @@ function playAgain(event, first, second) {
   $("#scoreTwo").replaceWith(
     `<p id='scoreTwo'>${second.name}: ${second.wins}</p>`
   );
-  if (second.name === "Computer" && first.turn === false) {
+  if (second.name === "Computer" && second.turn === true) {
     computerPlay(first, second);
   }
 }
+//this function run if someone win, display his name, his score and play wining sound
 function winar(player) {
   let win = $("#win");
   win.trigger("play");
@@ -120,29 +127,32 @@ function winar(player) {
     }
   });
 }
-///////Not working
-let possibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-function tryToWin(second) {
+
+//this one return the missing winning pattrin i use it to close the way of player one or to win.
+function tryToWin(playerSstack) {
   console.log("try to win ");
   match = 0;
   for (let win of winPattrens) {
-    for (let i = 0; i < second.stack.length; i++) {
-      if (win.includes(second.stack[i])) {
+    for (let i = 0; i < playerSstack.stack.length; i++) {
+      if (win.includes(playerSstack.stack[i])) {
         match++;
         if (match === 2) {
           console.log("Win in bad ", win);
-          console.log("X stack is ", second.stack);
+          console.log("O stack is ", playerSstack.stack);
 
-          console.log("win !U X is ", win.diff(second.stack)[0]);
-          if (played.includes(win.diff(second.stack)[0])) {
-            console.log("X stack", second.stack);
+          console.log("win !U O is ", win.diff(playerSstack.stack)[0]);
+          if (played.includes(win.diff(playerSstack.stack)[0])) {
+            console.log("O stack", playerSstack.stack);
             console.log("PLayed ", played);
             console.log("continue at ", win);
             continue;
           } else {
-            console.log("X stack", second.stack);
-            console.log("played not includes ", win.diff(second.stack)[0]);
-            return win.diff(second.stack)[0];
+            console.log("O stack", playerSstack.stack);
+            console.log(
+              "played not includes ",
+              win.diff(playerSstack.stack)[0]
+            );
+            return win.diff(playerSstack.stack)[0];
           }
         }
       }
@@ -150,66 +160,83 @@ function tryToWin(second) {
     match = 0;
   }
 }
-function imBad(first) {
-  console.log("Bad ");
-  match = 0;
-  for (let win of winPattrens) {
-    for (let i = 0; i < first.stack.length; i++) {
-      if (win.includes(first.stack[i])) {
-        match++;
-        if (match === 2) {
-          console.log("Win in bad ", win);
-          console.log("X stack is ", first.stack);
+// function imBad(first) {
+//   console.log("Bad ");
+//   match = 0;
+//   for (let win of winPattrens) {
+//     for (let i = 0; i < first.stack.length; i++) {
+//       if (win.includes(first.stack[i])) {
+//         match++;
+//         if (match === 2) {
+//           console.log("Win in bad ", win);
+//           console.log("X stack is ", first.stack);
 
-          console.log("win !U X is ", win.diff(first.stack)[0]);
-          if (played.includes(win.diff(first.stack)[0])) {
-            console.log("X stack", first.stack);
-            console.log("PLayed ", played);
-            console.log("continue at ", win);
-            continue;
-          } else {
-            console.log("X stack", first.stack);
-            console.log("played not includes ", win.diff(first.stack)[0]);
-            return win.diff(first.stack)[0];
-          }
-        }
-      }
-    }
-    match = 0;
-  }
-}
+//           console.log("win !U X is ", win.diff(first.stack)[0]);
+//           if (played.includes(win.diff(first.stack)[0])) {
+//             console.log("X stack", first.stack);
+//             console.log("PLayed ", played);
+//             console.log("continue at ", win);
+//             continue;
+//           } else {
+//             console.log("X stack", first.stack);
+//             console.log("played not includes ", win.diff(first.stack)[0]);
+//             return win.diff(first.stack)[0];
+//           }
+//         }
+//       }
+//     }
+//     match = 0;
+//   }
+// }
+/* */
+
+//this one try to win first if not possible try to close the way of the other player
 function smart(first, second) {
   playCo = tryToWin(second);
   if (playCo === undefined) {
-    playCo = imBad(first);
-    if (playCo === undefined) {
-      if (!played.includes("1")) playCo = "1";
-      else playCo = dumy();
-    }
+    playCo = tryToWin(first);
   }
   return playCo;
 }
 
+//I use this to find the empty squares
+let possibles = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//to play in any possible place
 function dumy() {
   console.log("dummy..........");
   canPlay = possibles.diff(played);
   canPlay = canPlay[Math.floor(Math.random() * canPlay.length)];
   return canPlay;
 }
+//this function will make the computer play
 function computerPlay(first, second) {
   if (computer.level === "easy") {
     canPlay = dumy();
-  } else if (computer.level === "medium") {
-    if (!played.includes("5")) {
-      canPlay = "5";
-      console.log("this one 5");
-    } else canPlay = smart(first, second);
-
+  } else if (computer.level === "hard") {
+    canPlay = smart(first, second);
+    if (canPlay === undefined) {
+      if (!played.includes("5")) {
+        canPlay = "5";
+        console.log("this one 5");
+      } else if (
+        (first.stack.includes("3") && first.stack.includes("7")) ||
+        (first.stack.includes("1") && first.stack.includes("9"))
+      ) {
+        console.log("woooooow222");
+        canPlay = "2";
+      } else if (!played.includes("1")) {
+        console.log("woooooow11");
+        canPlay = "1";
+      } else {
+        canPlay = dumy();
+      }
+    }
     console.log("Here is array ", canPlay);
   }
 
   if (!second.stack.includes(canPlay)) {
     $(`#${canPlay}`).addClass("o");
+    woosh.trigger("play");
     second.stack.push(canPlay);
     played.push(canPlay);
   } else {
@@ -255,9 +282,9 @@ function firstPlay(event, first, second, played) {
     second.turn = true;
   }
 }
-function secondPlay(event, first, second, played) {
+function secondPlay(event, first, second) {
   let square = event.target;
-  let woosh = $("#woosh");
+
   if ($(square).hasClass("x") || $(square).hasClass("o")) {
     console.log("play some thing else");
   } else {
@@ -274,16 +301,20 @@ function secondPlay(event, first, second, played) {
   console.log("Turn changed to player one");
   first.turn = true;
 }
+
 function play(event, first, second) {
-  gameOver = false;
-  if (firstPlayer.turn) {
+  if (first.turn) {
     firstPlay(event, first, second, played);
     console.log("Player One played");
   } else {
     secondPlay(event, first, second);
     console.log("Player Two played");
   }
-  if (second.name === "Computer" && gameOver === false) {
+  if (
+    second.name === "Computer" &&
+    second.turn === true &&
+    gameOver === false
+  ) {
     computerPlay(first, second, played);
     console.log("Computer played");
   }
@@ -291,7 +322,7 @@ function play(event, first, second) {
 
 let firstPlayer = playerOne;
 let secondPlayer = playerTwo;
-
+//add a new function to the array object to get the different between two array
 Array.prototype.diff = function(a) {
   return this.filter(function(i) {
     return a.indexOf(i) < 0;
@@ -313,6 +344,13 @@ $("#myForm input").on("change", function() {
     console.log(secondPlayer.name);
   }
 });
+
+//to change the level of the computer
+$("#computerLevel input").on("change", function() {
+  let level = $("input[name=level]:checked", "#computerLevel").val();
+  computer.level = level;
+});
+
 let playArea = $(".playing");
 playArea.on("click", function() {
   play(event, firstPlayer, secondPlayer, played);
@@ -320,10 +358,10 @@ playArea.on("click", function() {
 
 let start = $("#start");
 start.on("click", function() {
-  startGame(event, firstPlayer, secondPlayer);
+  startGame(firstPlayer, secondPlayer);
 });
 
 let again = $("#playAgain");
 again.on("click", function() {
-  playAgain(event, firstPlayer, secondPlayer, played);
+  playAgain(firstPlayer, secondPlayer);
 });
